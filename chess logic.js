a8a92fce96f517f62a87460ba6db9b2ead3079f0
Nick McClorey <mcclorns@mail.uc.edu>
2018-02-03 00:12:48 -0500

    added a function to select next piece to move. the function in charge of moving was modified to accept an object as a parameter so it can move whichever piece was selected

--- chess logic.js
+++ chess logic.js
@@ -29,13 +29,21 @@ var bishop1 = new bishop_setup(2,2,id,'bishop');
 id=id+1;
 var bishop2 = new bishop_setup(3,3,id,'bishop');
 
+function pieceById(id) {
+
+  if (id == 1) {return bishop1;}
+  else {return bishop2;}
+}// of function pieceById
+
+
 //document.write("connected");
 // used to place an piece on the board
 function place(row,col,inner) {
   board.rows[row].cells[col].innerHTML = inner;
 }
 place(2,2,bishop1.inner);
-place(2,3,bishop2.inner);
+place(3,3,bishop2.inner);
+
 
 
 // removes a piece from the board
@@ -88,14 +96,47 @@ function find2(piece){
 }// of function find
 
 
+var piece_selected = 0;
+var message_board = document.getElementById('message_board');
+
+
+function select(keep,piece_selected) {
+  for (row=0; row < 8; row++) {
+    for(col=0; col < 8; col++) {
+      if (board.rows[row].cells[col].innerHTML == '_') {
+          board.rows[row].cells[col].innerHTML = keep;
+          message_board.innerHTML=(piece_selected+" is selected");
+      }// of inner if
+    }// of outer if
+  }// of inner for
+}// of inner for
+
+
+
 
 k=0;
 for (i=0;i<8;i++) {
   for (k=0;k<8;k++) {
     board.rows[i].cells[k].addEventListener('click',function() {
-      this.innerHTML = '_';
-      find2(bishop2);
-    }
+      if (gameState == 'toSelect') {
+        if (this.firstChild.id) {
+          var keep = this.innerHTML;
+          piece_selected = this.firstChild.id;
+          this.innerHTML = '_';
+          select(keep,piece_selected);
+          gameState = 'selected';
+        }
+
+      } else if (gameState == 'selected') {
+        this.innerHTML = '_';
+        gameState = 'toSelect';
+        document.getElementById('message_board').textContent = gameState;
+        find2(pieceById(piece_selected));
+      }
+    }// of function in event listener
   ,false);
   }// of inner for
 }// of outer loop
+
+
+document.write(board.rows[3].cells[3].firstChild.id);

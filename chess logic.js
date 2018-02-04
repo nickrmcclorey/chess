26b3941d4bc33fbb557156b8bab8a5237d9e4776
Nick McClorey <mcclorns@mail.uc.edu>
2018-02-03 21:29:00 -0500

    its working alright and Im doing this before I make major changes

--- chess logic.js
+++ chess logic.js
@@ -1,80 +1,7 @@
-var board = document.getElementById('board');// connects us to the board
-var gameState = 'toSelect';
-var turn = 'white'
-
-// setting up the boards colors
-for (k=0; k<8; k=k+2) {
-  for (i=0; i<8; i=i+2) {
-    board.rows[k].cells[i].style.background="black";
-  }// of first inner for
-
-  for (i=0; i<8; i=i+2) {
-    board.rows[k+1].cells[i+1].style.background="black";
-
-  }// of second inner loop
-}// of outer for
 
 
-function bishop_setup(x_init,y_init,id,type) { // this is the class for pieces
-  this.type = type;
-  this.xPos = x_init;
-  this.yPos = y_init;
-  this.type = 'bishop';
-  this.inner = '<img src="pieces/bishop.jpg" id =" '+ id +' " width="70" height="70"></img>';
+function move_piece(piece){
 
-}// of function bishop_setup
-
-var id = 1;
-var bishop1 = new bishop_setup(2,2,id,'bishop');
-id=id+1;
-var bishop2 = new bishop_setup(3,3,id,'bishop');
-
-function pieceById(id) {
-
-  if (id == 1) {return bishop1;}
-  else {return bishop2;}
-}// of function pieceById
-
-
-//document.write("connected");
-// used to place an piece on the board
-function place(row,col,inner) {
-  board.rows[row].cells[col].innerHTML = inner;
-}
-place(2,2,bishop1.inner);
-place(3,3,bishop2.inner);
-
-
-
-// removes a piece from the board
-function remove(row,col) {
-  board.rows[row].cells[col].innerHTML = '';
-}
-
-
-function find(){
-  for (row=0; row < 8; row++) {
-    for(col=0; col < 8; col++) {
-      if (board.rows[row].cells[col].innerHTML == '_') {
-        if ( ((col-bishop1.xPos == row-bishop1.yPos) || (col-bishop1.xPos == bishop1.yPos-row)) && (col-bishop1.xPos !== 0) ) {
-          place(row,col,bishop1.inner);
-          remove(bishop1.yPos,bishop1.xPos);
-          bishop1.yPos=row;
-          bishop1.xPos=col;
-          if (board.rows[0].cells[0].firstChild.id) {
-            document.write(board.rows[2].cells[3].firstChild.id);
-            document.write(board.rows[0].cells[0].firstChild.id);
-          } ;
-        } else { // erase the marker
-          board.rows[row].cells[col].innerHTML = '';
-        }// of inner if
-      }// of outer if
-    }// of inner for
-  }// of inner for
-}// of function find
-
-
-function find2(piece){
   for (row=0; row < 8; row++) {
     for(col=0; col < 8; col++) {
       if (board.rows[row].cells[col].innerHTML == '_') {
@@ -88,7 +15,7 @@ function find2(piece){
             document.write(board.rows[0].cells[0].firstChild.id);
           } ;
         } else { // erase the marker
-          board.rows[row].cells[col].innerHTML = '';
+          board.rows[row].cells[col].innerHTML = keep;
         }// of inner if
       }// of outer if
     }// of inner for
@@ -105,7 +32,7 @@ function select(keep,piece_selected) {
     for(col=0; col < 8; col++) {
       if (board.rows[row].cells[col].innerHTML == '_') {
           board.rows[row].cells[col].innerHTML = keep;
-          message_board.innerHTML=(piece_selected+" is selected");
+          message_board.innerHTML=(piece_selected+" is selected. Its at "+piece_selected.xPos+" "+piece_selected.yPos);
       }// of inner if
     }// of outer if
   }// of inner for
@@ -128,10 +55,11 @@ for (i=0;i<8;i++) {
         }
 
       } else if (gameState == 'selected') {
+        var keep = this.innerHTML;
         this.innerHTML = '_';
         gameState = 'toSelect';
         document.getElementById('message_board').textContent = gameState;
-        find2(pieceById(piece_selected));
+        move_piece(pieceById(piece_selected,keep));
       }
     }// of function in event listener
   ,false);
@@ -139,4 +67,5 @@ for (i=0;i<8;i++) {
 }// of outer loop
 
 
-document.write(board.rows[3].cells[3].firstChild.id);
+var twod =[ [1,2,3],[4,5,6] ];
+document.write(twod[1][1]);
---
+++ setup.js
@@ -0,0 +1,64 @@
+var board = document.getElementById('board');// connects us to the board
+var gameState = 'toSelect';
+var turn = 'white'
+
+
+// setting up the boards colors
+for (k=0; k<8; k=k+2) {
+  for (i=0; i<8; i=i+2) {
+    board.rows[k].cells[i].style.background="black";
+  }// of first inner for
+
+  for (i=0; i<8; i=i+2) {
+    board.rows[k+1].cells[i+1].style.background="black";
+
+  }// of second inner loop
+}// of outer for
+
+
+function place(row,col,inner) {
+  board.rows[row].cells[col].innerHTML = inner;
+}
+
+
+function bishop_setup(x_init,y_init,id,type) { // this is the class for pieces
+  this.type = type;
+  this.xPos = x_init;
+  document.write(this.xPos);
+  this.yPos = y_init;
+  this.type = 'bishop';
+  this.inner = '<img src="pieces/bishop.jpg" id =" '+ id +' " width="70" height="70"></img>';
+  place(y_init,x_init,this.inner);
+
+}// of function bishop_setup
+
+var id = 1;
+var bishop1,bishop2,bishop3,bishop4;
+bishop1 = new bishop_setup(2,2,id,'bishop');
+id++;
+bishop2 = new bishop_setup(2,3,id,'bishop');
+id++;
+bishop3 = new bishop_setup(5,5,id,'bishop');
+
+
+
+var allPieces = [bishop1, bishop2, bishop3, bishop4];
+function pieceById(id) {
+  return allPieces[id-1];
+//document.write(id);
+
+
+}// of function pieceById
+
+
+//document.write("connected");
+// used to place an piece on the board
+//place(2,2,bishop1.inner);
+//place(3,3,bishop2.inner);
+
+
+
+// removes a piece from the board
+function remove(row,col) {
+  board.rows[row].cells[col].innerHTML = '';
+}

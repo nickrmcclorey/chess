18cfb2f24e6c510cee0cc76cc1c0ebc8219bc0d8
Nick McClorey <mcclorns@mail.uc.edu>
2018-02-04 05:50:20 -0500

    sharpened up the function isValidBishop

--- chess_functions.js
+++ chess_functions.js
@@ -30,7 +30,7 @@ function movePiece(row,col) {
   // update selected_piece's value
   gameBoard[row][col].yPos = row;
   gameBoard[row][col].xPos = col;
-  gameState = 'toSelect'
+  gameState = 'toSelect';
   changeTurn(); // change the turn
 
 }
@@ -40,33 +40,44 @@ function movePiece(row,col) {
 
 
 
-
-function isValidBishop(row,col,spot) {
+// checks to see if a bishop can do a certain move
+function isValidBishop(row,col) {
+    document.getElementById('cnn').textContent=(col-selected_piece.xPos);
     var distance = abs(col-selected_piece.xPos);
     if (abs(row-selected_piece.yPos) != abs(col-selected_piece.xPos)) {
 
       return 0;
-      // else if below is for moving down right
-    } else if ((col-selected_piece.xPos > 0)  && (row-selected_piece.yPos > 0)) {
-      var counter = 1;
-      document.getElementById('cnn').textContent=distance;
-      while (counter < distance) {
-        if ( gameBoard[selected_piece.yPos+counter][selected_piece.xPos+counter] != 0 ) {
-
-          //document.write(selected_piece.xPos+counter);
-            return 0;
-        }
-        counter++;
-    }// of while loop
-    return 1; // means none of the bad conditons were met so proceed
 
 
-    //else below is for moving down and left
-  } else if ((col-selected_piece.xPos < 0)  && (row-selected_piece.yPos > 0)) {
-    var counter = 1;
+    } else if (distance == 1) {
+      return 1;
+    // checking for pieces in the way. you can't go through someone
+    } else {
+    var ycounter = 0;
+    var xcounter = 0;
+    var counter = 0;
     document.getElementById('cnn').textContent=distance;
+    // loops through all the spots between where the piece is and where it
+    // wants to go, checking to see if anyone is there, blocking the path
     while (counter < distance) {
-      if ( gameBoard[selected_piece.yPos+counter][selected_piece.xPos-counter] != 0 ) {
+      // these if else branches increment the ycounter and xcounter in a way
+      // that all the spots in between will be checked
+      if ((col-selected_piece.xPos < 0) && (row-selected_piece.yPos > 0)) {
+        ycounter++;
+        xcounter=xcounter-1;
+      } else if ((col-selected_piece.xPos < 0) && (row-selected_piece.yPos) < 0) {
+        ycounter--;
+        xcounter--;
+
+      } else if ((col-selected_piece.xPos > 0) && (row-selected_piece.yPos) < 0) {
+        ycounter--;
+        xcounter++;
+      } else if ((col-selected_piece.xPos > 0)  && (row-selected_piece.yPos > 0)) {
+        ycounter++;
+        xcounter++;
+      }
+      // checks to see if a piece is in a spot in the way before continuing
+      if ( gameBoard[selected_piece.yPos+ycounter][selected_piece.xPos+xcounter] != 0) {
 
         //document.write(selected_piece.xPos+counter);
           return 0;
@@ -78,11 +89,17 @@ function isValidBishop(row,col,spot) {
 }// of function isValidBishop
 
 
+//checks to see if a knight is allowed to do a move
+//function isValidKnight(row,col)
+
+
+
+
 
 function moveIsValid(row,col) {
   // make sure they don't move to a teammates postion
   var spot = gameBoard[row][col];
-  if ( ((selected_piece.xPos == col) && (selected_piece.yPos == row)) || spot.team== selected_piece.team) {
+  if ( ((selected_piece.xPos == col) && (selected_piece.yPos == row)) || (spot.team == selected_piece.team)) {
     return 0;
   }
 
